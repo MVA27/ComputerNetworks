c7bec4780adb46c58a08568f297024056d775698
Krishna <65461756+MVA27@users.noreply.github.com>
2021-04-11 18:58:57 +0530

    receiver() implemented

--- HammingCode/HammingCodeByMehul.cpp
+++ HammingCode/HammingCodeByMehul.cpp
@@ -1,7 +1,17 @@
+//Hamming Code based on Even Parity
 #include<iostream>
-#include<math.h>	// For pow()
+#include<math.h>	 // For pow()
 #include<cstdlib>	// For abs()
 using namespace std;
+
+void display(int arr[],int size)
+{
+	for(int i = 0 ; i< size ; i++)
+	{
+		cout<<arr[i]<<"\t";
+	}
+	cout<<endl;
+}
 
 void sender()
 {
@@ -22,13 +32,8 @@ void sender()
 	cout<<endl;
 
 	//Display The Data
-	cout<<"Data : ";
-	for(int i = 0 ; i< sizeofData ; i++)
-	{
-		cout<<dataArray[i]<<"\t";
-	}
-	cout<<endl;
-
+	cout<<"Data From User: ";
+	display(dataArray,sizeofData); 
 	
 	//Calculate Number of redundant bits to be appended
 	int redundantBits;
@@ -59,12 +64,8 @@ void sender()
 	}
 	
 	
-	cout<<"Code word : ";
-	for(int i = 0 ; i< finalSize ; i++)	
-	{
-		cout<<codeword[i]<<"\t";
-	}
-	cout<<endl;
+	cout<<"Code word Only With Redundant Bits : "<<endl;
+	display(codeword,finalSize);
 	
 	
 	//Now where every there is no negative sign put the user data bits
@@ -79,12 +80,8 @@ void sender()
 	}
 	cout<<endl;
 
-	cout<<"Intermediate Code word : "<<endl;
-	for(int i = 0 ; i< finalSize ; i++)	
-	{
-		cout<<codeword[i]<<"\t";
-	}
-	cout<<endl;
+	cout<<"Code word With Redundant Bits and Data bits : "<<endl;
+	display(codeword,finalSize);
 	
 	
 	//=============PHASE 3=================
@@ -149,19 +146,139 @@ void sender()
 	}
 
 	//=============PHASE 4=================
-	//Final codeword[] is 
-	cout<<"Final Code word : "<<endl;
-	for(int i = 0 ; i< finalSize ; i++)
+	cout<<"Final Code word That Will be sent to Receiver : "<<endl;
+	display(codeword,finalSize);
+}
+
+
+void receiver()
+{
+	// PHASE 1 :: Take Size of Codeword
+	int sizeofCodeword;
+	cout<<"Enter The Size Of Code word : ";
+	cin>>sizeofCodeword;
+	cout<<endl;
+
+	//Enter The Code word
+	int codeword[sizeofCodeword];
+	cout<<"Enter The "<<sizeofCodeword<<" bits Of Data : ";
+	for(int i = 0 ; i< sizeofCodeword ; i++)
 	{
-		cout<<codeword[i]<<"\t";
+		cin>>codeword[i];
 	}
 	cout<<endl;
+	
+	display(codeword,sizeofCodeword);
+	
+	
+	// PHASE 2 :: Detect Error ie Locate the redundant/parity bits and check its parity
+	int pIndex = 0,pointer = 0;
+	bool isEven = false,isOdd = false; //Will determine if a redundent bit is Even of Odd parity
+	int numberOfParityBits = 0,parityArray[100];
+	for(int i = 0 ; i<sizeofCodeword ; i++)	//Infinite Loop
+	{
+		pIndex = pow(2,i);	//Determine power of 2 value (which gives executeTill value)
+		pointer = pIndex-1;	//Determine actual index  (which gives starting index value for loop)
+		
+		
+		if(pIndex > sizeofCodeword)	//If power of 2 is > size of whole codeword then break
+		{
+			break;
+		}
+
+		numberOfParityBits++; //Will count number of parity bits in codeword[]
+
+		int executeTill = pIndex,numberOfOnes = 0;
+		int counter = 0; //var counter to increment uptil var executeTill
+		
+		
+		while(pointer < sizeofCodeword)
+		{
+			//start from current bit and consider next bits upto executeTill and till pointer is not pointing outside codeword[]
+			for(pointer ; counter < executeTill && pointer < sizeofCodeword ; pointer++) 
+			{
+				
+				//cout<<endl<<"conside : "<<codeword[pointer]<<"at index = "<<pointer+1<<"\t";
+				if(codeword[pointer] == 1)
+				{
+					numberOfOnes++;
+				}
+	
+				counter++;
+			}		
+			counter = 0;
+			cout<<endl;
+
+			//start from current bit and skip next bits upto executeTill and till pointer is not pointing outside codeword[]
+			for(pointer ; counter < executeTill && pointer < sizeofCodeword ; pointer++)
+			{
+				counter++;
+			}
+			counter = 0;	
+			cout<<endl;
+		}
+		
+		if(numberOfOnes % 2 == 0)//Even number of 1s exists. (Therefore put 0 to keep it Even Theoritically)
+		{
+			isEven = true;	//Currenty has no purpose. But will be helpful if program is changed to support Odd parity
+			parityArray[numberOfParityBits-1] = 0;
+			cout<<"EVEN"<<endl;
+		}
+		
+		else	//Odd number of 1s exists. (Therefore put 1 to make it Even Theoritically)
+		{
+			isOdd = true;	//A bit contains odd number of 1s, Therefore make a note of it
+			parityArray[numberOfParityBits-1] = pIndex;
+			cout<<"ODD"<<endl;
+		}
+	
+		
+		//Check if even one redundant bit has Odd parity
+		if(isOdd == true)
+		{
+			//Current bit contains Odd number of 1s. Therefore our Assumption is wrong and msg contains ERROR
+			cout<<"**ERROR DETECTED**"<<endl;
+		}
+		
+		else{
+			cout<<"Message is ERROR FREE"<<endl;
+		}
+	}
+
+
+	//PHASE 3 :: Correct Error
+	cout<<endl<<"No. of P Bits = "<<numberOfParityBits<<endl;
+	display(parityArray,numberOfParityBits);
+	
+	cout<<endl<<"codeword before correction"<<endl;
+	display(codeword,sizeofCodeword);
+	
+	int errorBit = 0;
+	for(int i = 0 ; i<numberOfParityBits ; i++)
+	{
+		errorBit = errorBit + parityArray[i];
+	}
+	
+	if(codeword[errorBit-1])
+	{
+		codeword[errorBit-1] = 0;
+	}
+	
+	else{
+		codeword[errorBit-1] = 1;
+	}
+	
+	cout<<endl<<"codeword After correction"<<endl;
+	display(codeword,sizeofCodeword);
+	
 }
+
 
 int main()
 {
 
-	sender();
+	//sender();
+	receiver();
 
 	return 0;
 }
